c6e31214a08f757fbe0cff997ac1740281b4a23d
weder messias da silva pereira <238622024@eniac.com>
2026-08-18 21:15:00 -0300

    paginas ferias concluida

--- app.js
+++ app.js
@@ -9,18 +9,20 @@ app.set('view engine', 'ejs');
 
 // Permite acessar arquivos da pasta public
 app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'views', 'login')));
 app.use(express.static(path.join(__dirname, 'views', 'Ferias')));
 
 app.get('/', (req, res) => {
-    res.render('login');
+    res.sendFile(path.join(__dirname, 'views', 'login', 'index.html'));
 });
 
 app.get('/ferias', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'Ferias', 'ferias.html'));
 });
 
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(authRoutes);
+app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => {
     console.log(`Evolua+ rodando na porta ${PORT}`);
